1fc7fc97bcd341b0b24cd317bef876ff62bdce69
Kevin Whalen <KWhalen2@kent.edu>
2014-03-16 17:04:15 -0400

    removed old

--- source/locate_func.java
+++ source/locate_func.java
@@ -47,42 +47,6 @@ private static void findFuncCalls(String fileName)
 		// visit and print the methods names
 		new MethodVisitor().visit(cu, null);
 
-/*
-		BufferedReader inFile = new BufferedReader(new FileReader(fileName));
-
-		// Function name pattern matching
-		Pattern funcStart = Pattern.compile("([a-zA-Z_]{1}[a-zA-Z_0-9]+\\s*\\()");
-		Pattern funcEnd = Pattern.compile("(\\))");
-
-		Integer count = 1;
-		while (inFile.ready()){
-			String line = inFile.readLine();
-
-			Matcher s = funcStart.matcher(line);
-			while (s.find()){
-				System.out.print("line: " + count.toString());
-				System.out.println(" " + s.group(1).toString());
-				Matcher e = funcEnd.matcher(line);
-				if (e.find(s.end(1))){
-					System.out.print("location of ) ");
-					System.out.println(e.end(1));
-					if (e.end(1) < line.length()){
-						if (line.charAt(e.end(1)) == '{'){
-							System.out.println("Found method implementation.");
-						}
-					}
-				}
-			}
-
-//			ArrayList<String> lineData= 
-//				new ArrayList<String>(Arrays.asList(line.split(",")));
-//			for (String s : lineData) s = s.trim();
-//
-//			data.add(lineData);
-
-			count++;
-		}
-*/
 	} catch (IOException ioe){
 		System.out.println("Input/Output error.");
 		System.exit(1);
