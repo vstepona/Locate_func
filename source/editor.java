55a8cbe739ecaa8966c732d34973be5a4452958c
Kevin Whalen <KWhalen2@kent.edu>
2014-05-04 21:56:13 -0400

    workspace files

---
+++ source/editor.java
@@ -0,0 +1,21 @@
+/* editor.java
+ * Rich text editor window.
+ * http://docs.oracle.com/javase/tutorial/uiswing/components/text.html
+ * http://docs.oracle.com/javase/tutorial/uiswing/components/editorpane.html
+ * http://docs.oracle.com/javase/7/docs/api/javax/swing/JEditorPane.html
+ * http://docs.oracle.com/javase/7/docs/api/java/awt/package-summary.html
+ * 2014-03-16
+*/
+
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+//import javax.swing.JEditorPane;
+
+class SourceEditor {
+
+
+
+}
+
